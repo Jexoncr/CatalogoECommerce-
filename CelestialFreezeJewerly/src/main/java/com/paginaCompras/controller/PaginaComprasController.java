db6cf2bedc3eb1629dfc53ffff35b0b948eb2609
Jexoncr <123706320+Jexoncr@users.noreply.github.com>
2026-03-09 21:13:36 -0600

    Add files via upload

---
+++ CelestialFreezeJewerly/src/main/java/com/paginaCompras/controller/PaginaComprasController.java
@@ -0,0 +1,153 @@
+package com.paginaCompras.controller;
+
+import com.paginaCompras.domain.PaginaComentarios;
+import com.paginaCompras.domain.PaginaCompras;
+import com.paginaCompras.service.PaginaComentariosService;
+import com.paginaCompras.service.PaginaComprasService;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/productos")
+
+public class PaginaComprasController {
+
+    @Autowired
+    private PaginaComprasService paginaComprasService;
+
+    @Autowired
+    private PaginaComentariosService paginaComentariosService;
+
+    //Devuelve la lista complete, la cual sera filtrada para descuentos.
+    @GetMapping(value = "/paginaComprasDescuento")
+    public String mostrarDescuento(Model model) {
+        var lista = paginaComprasService.getComprases();
+        model.addAttribute("productos", lista);
+        //model.addAttribute("totalProductos", lista.size());
+        return "/productos/paginaComprasDescuento";
+    }
+
+    //Muestra solo productos en tendencia
+    @GetMapping(value = "/paginaComprasTendencias")
+    public String mostrarTendencias(Model model) {
+        var lista = paginaComprasService.getComprases();
+
+        // Filtra solo los productos que están en tendencia y sean a igual "Si"
+        var productosEnTendencia = lista.stream()
+                .filter(producto -> "SI".equals(producto.getTendencias()))
+                .collect(Collectors.toList());
+
+        model.addAttribute("productos", productosEnTendencia);
+        return "/productos/paginaComprasTendencias";
+    }
+
+    //Muestra el catalogo completo
+    @GetMapping(value = "/paginaComprasCatalogo")
+    public String mostrarCatalogo(Model model) {
+        var lista = paginaComprasService.getComprases();
+        model.addAttribute("productos", lista);
+        //model.addAttribute("totalProductos", lista.size());
+        return "/productos/paginaComprasCatalogo";
+    }
+
+    //Procedimiento que recibe un String de parametro desde la barra de busqueda
+    @GetMapping(value = "/buscarProductos")
+    public String buscarProductos(@RequestParam(name = "buscarTendencia") String buscarTendencia, Model model) {
+        var lista = paginaComprasService.getComprases();
+
+        //Busca los productos que contengan el string buscado y que esten en tendencia
+        var productosBuscados = lista.stream()
+                .filter(producto -> producto.getNombreProducto().toLowerCase().contains(buscarTendencia.toLowerCase()) && "Si".equals(producto.getTendencias()))
+                .collect(Collectors.toList());
+
+        //Agrega al modelo los productos encontrados
+        model.addAttribute("productos", productosBuscados);
+        return "/productos/paginaComprasTendencias";
+    }
+
+    //Procedimiento para buscar en Descuentos
+    @GetMapping(value = "/buscarDescuentos")
+    public String buscarDescuentos(@RequestParam(name = "buscarDescuento") String buscarDescuento, Model model) {
+        var lista = paginaComprasService.getComprases();
+
+        //Busca el producto con el nombre y que tenga un descuento
+        var descuentosBuscados = lista.stream()
+                .filter(producto -> producto.getNombreProducto().toLowerCase().contains(buscarDescuento.toLowerCase())
+                && producto.getPrecioDescuento().compareTo(BigDecimal.ZERO) > 0)
+                .collect(Collectors.toList());
+        //Agrega al modelo los productos encontrados
+        model.addAttribute("descuentos", descuentosBuscados);
+        return "/productos/paginaComprasDescuento";
+    }
+
+    //Procedimiento para buscar en Catalogo
+    @GetMapping(value = "/buscarCatalogo")
+    public String buscarCatalogo(@RequestParam(name = "buscarProducto") String buscarProducto, Model model) {
+        var lista = paginaComprasService.getComprases();
+
+        //Busca el producto con el nombre
+        var productosBuscados = lista.stream()
+                .filter(producto -> producto.getNombreProducto().toLowerCase().contains(buscarProducto.toLowerCase()))
+                .collect(Collectors.toList());
+        //Agrega al modelo los productos encontrados
+        model.addAttribute("productos", productosBuscados);
+        return "/productos/paginaComprasCatalogo";
+    }
+
+    @GetMapping("/detalle/{id}")
+    public String mostrarDetalleProducto(@PathVariable("id") Long id, Model model) {
+        PaginaCompras producto = paginaComprasService.getComprasById(id);
+        List<PaginaComentarios> comentarios = paginaComentariosService.getComentarios(id);
+        model.addAttribute("producto", producto);
+        model.addAttribute("comentarios", comentarios);
+        return "/productos/detalleProducto";
+    }
+
+    //Muestra todos los productos
+    @GetMapping("/listado")
+    public String listado(Model model) {
+        var lista = paginaComprasService.getComprases();
+        model.addAttribute("productos", lista);
+        model.addAttribute("totalProductos", lista.size());
+        return "/productos/listado";
+    }
+
+    //Metodo para guardar/modificar productos
+    @GetMapping("/nuevo")
+    public String productoNuevo(PaginaCompras paginaCompras) {
+        return "/productos/modificaProducto";
+    }
+
+    @PostMapping("/guardar")
+    public String productoGuardar(PaginaCompras paginaCompras) {
+        //Ver si se mandan los datos
+        System.out.println("Guardando producto: " + paginaCompras);
+        paginaComprasService.save(paginaCompras);
+        return "redirect:/productos/listado";
+    }
+    
+   @GetMapping("/eliminar/{id}")
+public String eliminarProducto(@PathVariable("id") Long id, RedirectAttributes redirectAttributes) {
+    try {
+        PaginaCompras producto = new PaginaCompras();
+        producto.setIdProducto(id);
+        paginaComprasService.delete(producto);
+        redirectAttributes.addFlashAttribute("mensaje", "Producto eliminado exitosamente.");
+    } catch (Exception e) {
+        redirectAttributes.addFlashAttribute("error", "No se puede eliminar el producto porque tiene comentarios asociados.");
+    }
+    return "redirect:/productos/listado";
+}
+    
+}
---
+++ CelestialFreezeJewerly/src/main/java/com/paginaCompras/service/PaginaComprasServiceImpl.java
@@ -0,0 +1,44 @@
+package com.paginaCompras.service;
+
+import com.paginaCompras.domain.PaginaCompras;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.paginaCompras.dao.PaginaComprasDao;
+
+@Service
+public class PaginaComprasServiceImpl implements PaginaComprasService{
+    
+    @Autowired
+    private PaginaComprasDao paginaComprasDao;
+    
+    @Override
+    //Se setea como read only, ya que es una consulta.
+    @Transactional(readOnly = true)
+    
+    //Proceso para cargas los productos
+    public List<PaginaCompras> getComprases() {
+        //Mete los producto dentro de una lista y la devuelve.
+        var lista = paginaComprasDao.findAll();        
+        return lista;
+    }
+    
+    @Override
+    //Se setea como read only, ya que es una consulta.
+    @Transactional(readOnly = true)
+    //Proceso el cual carga el producto por id buscado
+    public PaginaCompras getComprases(PaginaCompras paginaCompras){
+        //Retorna el producto encontrado, si no encuentra, devuelve null.
+        return paginaComprasDao.findById(paginaCompras.getIdProducto()).orElse(null);
+    }
+    
+    
+    @Override 
+    //Se setea como read only, ya que es una consulta.
+    @Transactional(readOnly = true) 
+    //Proceso el cual carga el producto por id solicitado en la pagina.
+    public PaginaCompras getComprasById(Long id) { 
+        return paginaComprasDao.findById(id).orElse(null);
+    }
+}
